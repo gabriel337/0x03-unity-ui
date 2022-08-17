b9bf56c5556b3b2b323de0942faa27d785312be0
gabriel337 <g.david.lopezruiz@gmail.com>
2022-08-17 15:50:25 -0400

    adding task 4 files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private int score = 0;
+    
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+      void FixedUpdate()
+    {
+        if (Input.GetKey("up") || Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, speed);        
+        }
+        if (Input.GetKey("down") || Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -speed);
+        }
+        if (Input.GetKey("left") || Input.GetKey("a"))
+        {
+            rb.AddForce(-speed, 0, 0);
+        }
+        if (Input.GetKey("right") || Input.GetKey("d"))
+        {
+            rb.AddForce(speed , 0, 0);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            score++;
+            Debug.Log($"Score: {score}");
+            
+
+        }
+    }
+}
